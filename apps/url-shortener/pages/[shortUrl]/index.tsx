879e3a1e7274d10c412efca6e0557590dae10db5
jasonmichaels <jasonmichaelwhite@gmail.com>
2023-05-28 18:48:18 -0400

    CHANGED: [shortUrl] page w/ notes (e.g. TODOs)

--- apps/url-shortener/pages/[shortUrl]/index.tsx
+++ apps/url-shortener/pages/[shortUrl]/index.tsx
@@ -1,6 +1,16 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
+/**
+ * @description A component solely responsible for reading
+ * query?.shortUrl to determine where to forward the user.
+ * 
+ * @TODOs
+ *  - handle cases where an invalid `shortUrl` is given, if at all
+ *  - change from IIFE in `useEffect` to something a bit cleaner
+ *  - style the page, since it does load however briefly, based
+ *    on the user's preferred contrast settings to minimize flashes
+ */
 const CheckUrl = () => {
   const { query } = useRouter();
 
@@ -14,6 +24,10 @@ const CheckUrl = () => {
       const result = await response.json();
 
       if (result.longUrl) {
+        /**
+         * @NOTE Prelim analytics using document.requests to
+         * track how often a URL is used
+         */
         await fetch('/api/update-url', {
           method: 'POST',
           body: JSON.stringify({ id: result.id })
