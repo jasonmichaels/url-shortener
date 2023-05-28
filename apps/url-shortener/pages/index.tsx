08c69af67e7190cf5b06aa7bee2f8e13d2c25442
jasonmichaels <jasonmichaelwhite@gmail.com>
2023-05-28 18:30:01 -0400

    CHANGED: validation of URL (super basic) and form styling

--- apps/url-shortener/pages/index.tsx
+++ apps/url-shortener/pages/index.tsx
@@ -1,12 +1,17 @@
-import { useCallback, FormEvent, useState, useEffect, useMemo } from 'react';
-import { useRouter } from 'next/router';
+import { useCallback, FormEvent, useState, useMemo } from 'react';
 
 import styles from './index.module.scss';
+
+const urlPattern = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
 
 export default function Index() {
   const [longUrl, setLongUrl] = useState('');
   const [shortUrl, setShortUrl] = useState('');
-  const router = useRouter();
+  const [error, setError] = useState('');
+
+  const isUrlValid = useMemo(() => {
+    return urlPattern.test(longUrl);
+  }, [longUrl]);
 
   const handleChange = useCallback((e: any) => {
     setLongUrl(e.target.value);
@@ -15,7 +20,7 @@ export default function Index() {
   const handleSubmit = useCallback(async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    if (longUrl) {
+    if (isUrlValid) {
       try {
         const checkResponse = await fetch('/api/check-url', {
           method: 'POST',
@@ -39,10 +44,10 @@ export default function Index() {
           }
         }
       } catch (e: any) {
-        console.log(e.message);
+        setError(e.message);
       }
     }
-  }, [longUrl]);
+  }, [longUrl, isUrlValid]);
 
   const formattedShortUrl = useMemo(() => {
     if (shortUrl) {
@@ -58,14 +63,17 @@ export default function Index() {
         <h1 className={styles.heading}>Welcome to hURLy</h1>
         <form id="url-form" className={styles.form} onSubmit={handleSubmit}>
           <input name="url" value={longUrl} onChange={handleChange} placeholder="Enter a valid URL" />
-          <button type="submit" form="url-form">hURL me!</button>
+          <button type="submit" form="url-form" disabled={!isUrlValid}>hURL me!</button>
         </form>
+        {error ? (
+          <p className={styles.error}>{error}</p>
+        ) : null}
         {formattedShortUrl ? (
           <div className={styles.linkContainer}>
-          hURLy shortened to:&nbsp;
-          <div className={styles.link}>
-            <a href={formattedShortUrl} target="_blank">{formattedShortUrl}</a>
-          </div>
+            hURLy shortened to:&nbsp;
+            <div className={styles.link}>
+              <a href={formattedShortUrl} target="_blank">{formattedShortUrl}</a>
+            </div>
           </div>
         ) : null}
       </div>
